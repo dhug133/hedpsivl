ccb2173f08b7da48d47c15c944f30f8576e17dea
dhug133 <dhug133@gmail.com>
2016-04-15 15:05:34 +0000

    edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
     def index
          @posts = Post.paginate(page: params[:page], per_page: 5)
     end
     
     def show
-      @post = Post.find(params[:id])
     end
     
     def new
@@ -24,17 +25,19 @@ class PostsController < ApplicationController
     end
   
     def edit  
-      @post = Post.find(params[:id])
     end
     
     def update
-      @post = Post.find(params[:id])
-      @post.update(post_params)
-      redirect_to(post_path(@post))
+      if @post.update(post_params)
+        flash[:success] = "Post updated."
+        redirect_to posts_path
+      else
+        flash.now[:alert] = "Update failed.  Please check the form."
+        render :edit
+      end
     end
     
-    def destroy  
-      @post = Post.find(params[:id])
+    def destroy
       @post.destroy
       redirect_to posts_path
     end 
@@ -43,4 +46,15 @@ class PostsController < ApplicationController
     def post_params  
       params.require(:post).permit(:image, :caption)
     end
+    
+    def set_post
+      @post = Post.find(params[:id])
+    end
+    
+    def owned_post  
+      unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+      end
+    end  
 end
