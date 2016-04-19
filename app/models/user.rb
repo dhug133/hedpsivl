800d2f6c2425aa1559b95ad572b680208a9cde23
Nguyễn Ngọc Hưng <zzznightonlyzzz@gmail.com>
2016-04-19 12:52:10 +0000

    #user_profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { medium: '152x152#' }  
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts do
     resources :comments
   end
   resources :home_page
   root 'home_page#index'  
+  get ':user_name', to: 'profiles#show', as: :profile  
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile  
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile  
 end
