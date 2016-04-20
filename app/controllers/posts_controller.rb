a0723596b3b59ff46a51a9ee8af37f07458aad78
Nguyễn Ngọc Hưng <zzznightonlyzzz@gmail.com>
2016-04-20 09:34:50 +0000

    #seach_and_unlike

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  before_action :set_post, only: [:show, :edit, :update, :destroy, :like]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :like, :unlike]
   before_action :owned_post, only: [:edit, :update, :destroy]
     def index
          @posts = Post.paginate(page: params[:page], per_page: 5)
@@ -51,7 +51,16 @@ class PostsController < ApplicationController
           format.js
         end
       end
-    end  
+    end
+    
+    def unlike
+      if @post.unliked_by current_user
+        respond_to do |format|
+          format.html { redirect_to :back }
+          format.js
+        end
+      end
+    end
     private
 
     def post_params  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   get 'profiles/show'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts do
     resources :comments
     member do
       get 'like'
+      get 'unlike'
     end
   end
   resources :home_page
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   get ':user_name', to: 'profiles#show', as: :profile  
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile  
   patch ':user_name/edit', to: 'profiles#update', as: :update_profile  
+  get 'searchs/search', to: 'searchs#search'
 end
