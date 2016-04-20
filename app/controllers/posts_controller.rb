49d3bb4186b60a6156a0d2b56ed2c6c6ccf8541d
Nguyễn Ngọc Hưng <zzznightonlyzzz@gmail.com>
2016-04-20 15:27:29 +0000

    *fix_bug_like

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   before_action :owned_post, only: [:edit, :update, :destroy]
     def index
          @posts = Post.paginate(page: params[:page], per_page: 5)
+         @newposts = Post.all.order(created_at: :desc).limit(3)
     end
     
     def show
