73b488e3f1e0ef84c0d3db8cd8940f349dc9d5b5
dhug133 <dhug133@gmail.com>
2016-05-04 13:48:51 +0000

    fix vote_for

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
     acts_as_taggable_on :tags
+    acts_as_votable
     
     belongs_to :user, dependent: :destroy
     has_many :notifications, dependent: :destroy  
